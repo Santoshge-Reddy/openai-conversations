1199c71baf525e0be64cd816798b0a304db47eb2
Gedala <sgedala@deloitte.com>
2023-01-13 21:39:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const OPENAI_API_KEY = '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';
+
+const Chatbot = () => {
+  const [conversationId, setConversationId] = useState('');
+  const [conversationHistory, setConversationHistory] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleChange = event => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'https://cors-anywhere.herokuapp.com/https://api.openai.com/v1/conversations/',
+        {
+          prompt: input,
+          conversation_id: conversationId,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+          },
+        }
+      );
+
+      setConversationId(response.data.conversation_id);
+      setConversationHistory([...conversationHistory, input, response.data.text]);
+      setInput('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={handleChange} />
+        <button type="submit">Send</button>
+      </form>
+      <div>
+        {conversationHistory.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Chatbot;
